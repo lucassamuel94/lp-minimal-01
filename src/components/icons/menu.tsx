d8e8bd4f7acdb21ef1ea2f733d4020680aacc6b5
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-04-17 19:20:34 -0300

    component: rename component IconMenu

--- src/components/icons/menu.tsx
+++ src/components/icons/menu.tsx
@@ -1,6 +1,6 @@
 import { LucideProps } from 'lucide-react'
 
-export function IconExemplo(props: LucideProps) {
+export function IconMenu(props: LucideProps) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 39 30' {...props}>
       <g clipPath='url(#clip0_2513_1474)'>
